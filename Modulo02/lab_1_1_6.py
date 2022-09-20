835b20bcef7e5f27aff46d3804155da0006f3a7d
Almndrz02 <almendarizdanie456@gmail.com>
2022-09-20 08:41:44 -0500

    laboratorio 2 1 1 6

---
+++ Modulo02/lab_1_1_6.py
@@ -0,0 +1,9 @@
+'''
+Descripción: Programa muy facil de la función Print
+Autor: Daniel Almendariz
+Fecha: 20 sep 2022
+'''
+
+print("Hola mundo!!!")
+print("Soy Daniel")
+print("Bienvenido a Python")

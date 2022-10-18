dc67ec116fedba6c4b375943efd0cb944845f706
Almndrz02 <almendarizdanie456@gmail.com>
2022-10-18 09:18:10 -0500

    03

--- Unidad_02/02_list_dicts.py
+++ Unidad_02/02_list_dicts.py
@@ -36,7 +36,7 @@ Why did the list and dictionary contents not print?
 Fix the code and run the script again.
 """
 
-print('\n'.join(capitals["South Africa"]) )
+print( "South Africa: ",'\n'.join(capitals["South Africa"]) )
 """
 Why did you get an error for the 2nd capital of South Africa?
 Hint: Check the syntax for the index value.
---
+++ Unidad_02/03_personal-info.py
@@ -0,0 +1,8 @@
+
+nombre = input("Cual es tu nombre?" )
+apellido = input("Cual es tu apellido?" )
+localidad = input("Cual es tu localidad? ")
+edad = input("Cual es tu edad? ")
+space = ""
+
+print("Hola!!!",space,nombre,space,apellido,space,"vives en",space,localidad,space,"y tienes",space,edad,"años" )
